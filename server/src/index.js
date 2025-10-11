6463a8055ee1035652df98564fab4cf0bc8a940f
Ankit Singh <ankitsingh435517@gmail.com>
2025-10-11 22:21:14 +0530

    feat: add ping route

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5500;
 
+app.get("/ping", (_, res) => res.json({ message: "pong" }));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
