c1744f2da782599001a4b8ffa23c24babb2a4649
Ankit Singh <ankitsingh435517@gmail.com>
2025-11-15 21:35:32 +0530

    feat: register and login form ui and other enhancements added

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,14 +1,21 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster position="bottom-right" richColors />
+    </QueryClientProvider>
+  </StrictMode>
+);
