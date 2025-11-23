b05eed2b40f1b0f7bef22d5b969452901cec0312
Ankit Singh <ankitsingh435517@gmail.com>
2025-11-23 18:38:59 +0530

    chore: remove unused declarations

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,14 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import {
-  createContext,
-  StrictMode,
-  useEffect,
-  useState,
-  type Dispatch,
-  type SetStateAction,
-} from "react";
+import { createContext, StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import {
   QueryClient,
